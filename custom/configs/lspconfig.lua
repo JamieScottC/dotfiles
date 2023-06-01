3a170225138206377dd9e2903e8aca2245a40fec
Jamie Scott <scott.jams@gmail.com>
2023-05-31 22:54:13 -0400

    add custom files

---
+++ custom/configs/lspconfig.lua
@@ -0,0 +1,29 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+
+lspconfig.tailwindcss.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+lspconfig.astro.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+lspconfig.svelte.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+--[[ open diagnostics on cursor  ]]
+-- vim.o.updatetime = 250
+-- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]

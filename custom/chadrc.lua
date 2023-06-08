20ec68972cf1390cf8f53253ecafbf09fd9d9b3e
JamieScottC <scott.jams@gmail.com>
2023-06-08 15:21:41 -0400

    change visual highlight color

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -1,5 +1,19 @@
 local M = {}
-M.ui = { theme = 'catppuccin' }
+M.ui = {
+  changed_themes = {
+    catppuccin = {
+      base_30 = {
+        line = "#5b5975",
+      },
+      base_16 = {
+        base02 = "#765b78",
+        base08 = "#F38BA8",
+        base0F = "#F38BA8",
+      }
+    },
+  },
+  theme = 'catppuccin'
+}
 M.plugins = 'custom.plugins'
 M.mappings = require "custom.mappings"
 return M
